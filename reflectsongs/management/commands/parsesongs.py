8e021dd9debb1cb907cd9f9693f24cbeedcdf999
Matthew Sital-Singh <mattss@webappservices.com>
2019-05-03 16:04:06 +0100

    Delete non-songs, add option for skipping updates

--- reflectsongs/management/commands/parsesongs.py
+++ reflectsongs/management/commands/parsesongs.py
@@ -14,14 +14,21 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('songs_dir')
+        parser.add_argument(
+            '--skip-update',
+            action='store_true',
+        )
 
     def handle(self, *args, **options):
         songs_dir = options['songs_dir']
         p = Path(songs_dir)
-        for pathitem in p.glob('*.pro6'):
-            self.parse_item(pathitem)
+        paths = list(p.glob('*.pro6'))
+        total = len(paths)
+        for index, pathitem in enumerate(paths):
+            print(f'Item {index+1} of {total}')
+            self.parse_item(pathitem, update=not options['skip_update'])
 
-    def parse_item(self, pathitem):
+    def parse_item(self, pathitem, update=True):
         # Read the file
         with pathitem.open() as thefile:
             itemdata = thefile.read()
@@ -38,7 +45,12 @@ class Command(BaseCommand):
         # Check category
         category = tree.attrib.get('category')
         if category != 'Song':
-            # skip
+            # Delete if exists and no ccli number
+            items_to_delete = Song.objects.filter(
+                title=title, ccli_number__exact='')
+            for item in items_to_delete:
+                print('Removing non-song: {}'.format(item))
+                item.delete()
             return
 
         # Create, or get existing by name
@@ -62,12 +74,15 @@ class Command(BaseCommand):
                 changed = True
 
         # Grab video from songselect
-        if song.songselect_url and not song.youtube_url:
-            video_url = grab_songselect_video(song.songselect_url)
-            if video_url:
-                song.youtube_url = video_url
-                changed = True
+        if update:
+            if song.songselect_url and not song.youtube_url:
+                video_url = grab_songselect_video(song.songselect_url)
+                if video_url:
+                    print(f'Got video for {song}')
+                    song.youtube_url = video_url
+                    changed = True
                 # Pause before contacting songselect again
+                print('Pausing...')
                 sleep(3)
 
         if changed:
