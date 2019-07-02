f7f1c6092a084495719d274687c691f4b46b8d72
Matthew Sital-Singh <mattss@webappservices.com>
2019-07-02 20:49:31 +0100

    More explicit path for songs

--- reflectsongs/urls.py
+++ reflectsongs/urls.py
@@ -7,6 +7,6 @@ from reflectsongs.views import HomeView, SongList, SongView
 
 urlpatterns = [
     path('', HomeView.as_view()),
-    path('s/<slug:song_slug>/', SongView.as_view(), name='song-view'),
+    path('song/<slug:song_slug>/', SongView.as_view(), name='song-view'),
     path('songs/', SongList.as_view(), name='song-list'),
 ]
