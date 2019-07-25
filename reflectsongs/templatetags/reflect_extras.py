34585e318075e00c7f88478b1d1b16946d65fc61
Matthew Sital-Singh <mattss@webappservices.com>
2019-07-25 20:28:14 +0100

    Opengraph support

--- reflectsongs/templatetags/reflect_extras.py
+++ reflectsongs/templatetags/reflect_extras.py
@@ -1,4 +1,5 @@
 from django import template
+from django.conf import settings
 from django.urls import reverse
 
 register = template.Library()
@@ -9,3 +10,8 @@ def navactive(request, url):
     if reverse(url) in request.path:
         return "active"
     return ""
+
+
+@register.simple_tag
+def root_url():
+    return settings.ROOT_URL
