ddc894d0429ad1a0c1d68ba7e8c4dfc22c17ee07
Matthew Sital-Singh <mattss@webappservices.com>
2019-09-08 14:29:47 +0100

    Add util to maintain query string when batching

--- reflectsongs/templatetags/reflect_extras.py
+++ reflectsongs/templatetags/reflect_extras.py
@@ -15,3 +15,15 @@ def navactive(request, url):
 @register.simple_tag
 def root_url():
     return settings.ROOT_URL
+
+
+@register.simple_tag
+def relative_url(value, field_name, urlencode=None):
+    url = '?{}={}'.format(field_name, value)
+    if urlencode:
+        querystring = urlencode.split('&')
+        filtered_querystring = filter(
+            lambda p: p.split('=')[0] != field_name, querystring)
+        encoded_querystring = '&'.join(filtered_querystring)
+        url = '{}&{}'.format(url, encoded_querystring)
+    return url
